0b3e4f816652a928c1f94cf549f71d60be854038
JudyWu <judywu@softnext.com.tw>
2019-12-13 14:32:00 +0800

    change http to https

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-const currentSite = 'http://api.openweathermap.org/data/2.5/weather?'
-const foreCastSite = 'http://api.openweathermap.org/data/2.5/forecast?'
+const currentSite = 'https://api.openweathermap.org/data/2.5/weather?'
+const foreCastSite = 'https://api.openweathermap.org/data/2.5/forecast?'
 const APPID = '9c8dadba0d86fe1e366e1e7781cdfc59'
 const imgSrc = 'https://openweathermap.org/img/w/'
 
