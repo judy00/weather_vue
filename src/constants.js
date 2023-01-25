e063a5121931455f90193d335bfe38a8f23c56d4
JI SHIUAN <31308489+judy00@users.noreply.github.com>
2023-01-25 17:32:32 +0800

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 const currentSite = 'https://api.openweathermap.org/data/2.5/weather?'
 const foreCastSite = 'https://api.openweathermap.org/data/2.5/forecast?'
-const APPID = '9c8dadba0d86fe1e366e1e7781cdfc59'
+const APPID = 'a5eb98051c94ef09a6813dbfa4201474'
 const imgSrc = 'https://openweathermap.org/img/w/'
 
 export {currentSite, foreCastSite, APPID, imgSrc}
